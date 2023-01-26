7673ea5f0e5cd17144428482023c3a6e613e34f3
zer0who <eagle7449@naver.com>
2023-01-26 10:57:58 +0900

    slice_n_square_array

---
+++ programmers/lv2/slice_n_square_array.py
@@ -0,0 +1,9 @@
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right + 1):
+        if i // n < i % n:
+            answer.append(i % n + 1)
+        else:
+            answer.append(i // n + 1)
+    
+    return answer
