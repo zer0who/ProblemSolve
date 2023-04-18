9d3e95e50cc1ce5bd3bac5d48f3516817fec7411
김영후 <eagle7449@naver.com>
2023-04-18 16:04:53 +0900

    fix

--- BOJ/Math1/2745.py
+++ BOJ/Math1/2745.py
@@ -2,8 +2,8 @@ def main():
     num = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     answer = 0
     N, B = input().split()
-    for i in reversed(range(len(N))):
-        temp = num.index(N[i]) * (int(B)**i)
+    for i in range(len(N)):
+        temp = num.index(N[i]) * (int(B)**(len(N)-i-1))
         answer += temp
     print(answer)
     # print(int(N, int(B)))
