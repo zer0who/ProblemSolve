08cb767f3aaba411e8124a75c51f43de60df8c38
김영후 <eagle7449@naver.com>
2023-05-08 15:20:28 +0900

    4134.py

---
+++ python/BOJ/python/divisor&multiple&prime2/4134.py
@@ -0,0 +1,22 @@
+def isPrime(num: int) -> int:
+    if num == 0 or num == 1:
+        return False
+    for i in range(2, int(num**(1/2))+1):
+        if num % i == 0:
+            return False
+        
+    return True
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        num = int(input())
+        while True:
+            if (isPrime(num)):
+                print(num)
+                break
+            num += 1
+
+
+if __name__ == "__main__":
+    main()
