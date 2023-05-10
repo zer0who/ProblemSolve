da66f65d3ca14fd5f5d56986fbd2bbbebdb0ad3c
김영후 <eagle7449@naver.com>
2023-05-10 15:38:11 +0900

    25192.py

---
+++ python/BOJ/python/deepen2/25192.py
@@ -0,0 +1,21 @@
+import sys
+
+def main():
+    N = int(sys.stdin.readline())
+    ans = 0
+    chatted_user = {}
+    for _ in range(N):
+        string = sys.stdin.readline().rstrip()
+        if string == "ENTER":
+            chatted_user = {}
+            continue
+        if string in chatted_user:
+            continue
+        chatted_user[string] = 1
+        ans += 1
+            
+    
+    print(ans)
+
+if __name__ == "__main__":
+    main()
