a1da005209b87adad53fb3db12a530eed9f232bb
김영후 <eagle7449@naver.com>
2023-09-07 22:18:39 +0900

    큐

---
+++ java/BOJ/class2p/Main_실버4_10845_큐.java
@@ -0,0 +1,45 @@
+package BOJ.class2p;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_실버4_10845_큐 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> queue = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            switch (input[0]) {
+                case "push":
+                    queue.offer(Integer.parseInt(input[1]));
+                    break;
+                case "pop":
+                    if (!queue.isEmpty()) System.out.println(queue.poll());
+                    else System.out.println(-1);
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) System.out.println(1);
+                    else System.out.println(0);
+                    break;
+                case "front":
+                    if (!queue.isEmpty()) System.out.println(queue.peek());
+                    else System.out.println(-1);
+                    break;
+                case "back":
+                    if (!queue.isEmpty()) System.out.println(queue.peekLast());
+                    else System.out.println(-1);
+                    break;
+            }
+        }
+    }
+
+}
