fdfcb8812b2c65cab2e24243d989cdd58742e78e
김영후 <eagle7449@naver.com>
2023-09-07 22:41:08 +0900

    Hashing

---
+++ java/BOJ/class2pp/Main_브론즈2_15829_Hashing.java
@@ -0,0 +1,30 @@
+package BOJ.class2pp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_브론즈2_15829_Hashing {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        System.out.println(hash(L, input));
+
+    }
+
+    private static long hash(int L, String input) {
+        long res = 0;
+        for (int i = 0; i < L; i++) {
+            int num = input.charAt(i)-96;
+            long powNum = 1;
+            for (int j = 0; j < i; j++) {
+                powNum = (powNum*31)%1234567891;
+            }
+            res += (num*powNum);
+        }
+
+        return res%1234567891;
+    }
+
+}
