3d269a07747d58602cca961db4cf386fde4ea6a3
김영후 <eagle7449@naver.com>
2023-09-27 18:59:52 +0900

    주석

--- java/SSAFY/study/algo/week10/Main_실버1_14940_쉬운최단거리.java
+++ java/SSAFY/study/algo/week10/Main_실버1_14940_쉬운최단거리.java
@@ -12,7 +12,7 @@ public class Main_실버1_14940_쉬운최단거리 {
     static int[] dirRow = {-1, 1, 0, 0};    // 상, 하, 좌, 우
     static int[] dirCol = {0, 0, -1, 1};
 
-    static class Moving {
+    static class Moving {   // 움직이는 것을 저장하기 위한 객체
         int row;
         int col;
         int moves;
@@ -37,9 +37,9 @@ public class Main_실버1_14940_쉬운최단거리 {
         Moving moving = init();
         move(moving);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {   // 출력하기
             for (int j = 0; j < m; j++) {
-                if (land[i][j] == 1 && !isVisited[i][j]) sb.append(-1).append(" ");
+                if (land[i][j] == 1 && !isVisited[i][j]) sb.append(-1).append(" "); // 원래 갈 수 있는 땅인데(마지막에 1인 곳), 도달하지 못한 경우(visited가 false인 경우)는 -1로 출력
                 else sb.append(land[i][j]).append(" ");
             }
             sb.append("\n");
@@ -71,7 +71,7 @@ public class Main_실버1_14940_쉬운최단거리 {
         return moving;
     }
 
-    static boolean isOutedOrBlocked(int nextRow, int nextCol) {
+    static boolean isOutedOrBlocked(int nextRow, int nextCol) { // 범위 밖으로 가거나, 0인 곳인지 체크
         if ( ((0 <= nextRow && nextRow < n) && (0 <= nextCol && nextCol < m)) && land[nextRow][nextCol] != 0 ) return false;
 
         return true;
@@ -90,7 +90,7 @@ public class Main_실버1_14940_쉬운최단거리 {
                 if (isOutedOrBlocked(nextRow, nextCol) || isVisited[nextRow][nextCol]) continue;
                 land[nextRow][nextCol] = nowMoves + 1;
                 queue.offer(new Moving(nextRow, nextCol, nowMoves+1));
-                isVisited[nextRow][nextCol] = true;
+                isVisited[nextRow][nextCol] = true; // 방문 처리
             }
         }
     }
