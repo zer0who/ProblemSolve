b95671b7328627463df72b435a515fe729fa2314
김영후 <eagle7449@naver.com>
2023-12-03 16:49:36 +0900

    temp

---
+++ java/SSAFY/study/algo/week17/Main_실버3_23814_아저볶.java
@@ -0,0 +1,19 @@
+package SSAFY.study.algo.week17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_실버3_23814_아저볶 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long D = Long.parseLong(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long N = Long.parseLong(br.readLine());
+        long M = Long.parseLong(br.readLine());
+        long K = Long.parseLong(br.readLine());
+    }
+
+}
