08aa5905880801fe13180b84c8773a0ebd414fe2
김영후 <eagle7449@naver.com>
2024-01-23 08:58:21 +0900

    temp

--- java/SSAFY/study/algo/week24/Main_골드5_3967_골드스타.java
+++ java/SSAFY/study/algo/week24/Main_골드5_3967_골드스타.java
@@ -10,15 +10,23 @@ public class Main_골드5_3967_골드스타 {
 
     public static void main(String[] args) throws IOException {
         init();
+        dfs();
     }
 
-    static void init() throws IOException {
+    static void init() throws IOException { // . 과 x를 제외한 알파벳들은 우선 숫자로 저장
         magicStar = new String[5][9];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 5; i++) {
-            String[] row = br.readLine().split("");
-            magicStar[i] = row;
+            String row = br.readLine();
+            for (int j = 0; j < row.length(); j++) {
+                if (!String.valueOf(row.charAt(j)).equals("x") && !String.valueOf(row.charAt(j)).equals(".")) magicStar[i][j] = String.valueOf(row.charAt(j) - 64); // A부터 1이니 64만 빼줌
+                else magicStar[i][j] = String.valueOf(row.charAt(j));
+            }
         }
+    }
+
+    static void dfs() {
+
     }
 
 }
