87bcfe4e5fe58bb356f75c0d5aa1f689a4b9408f
김영후 <eagle7449@naver.com>
2024-01-24 09:23:19 +0900

    temp

--- java/SSAFY/study/algo/week24/Main_골드5_3967_골드스타.java
+++ java/SSAFY/study/algo/week24/Main_골드5_3967_골드스타.java
@@ -7,31 +7,42 @@ import java.util.Arrays;
 
 public class Main_골드5_3967_골드스타 {
 
-    static String[][] magicStar;    // 알파벳이 들어가는 인덱스: 0, 4 / 1, 1 . 1, 3 . 1, 5 . 1, 7 / 2, 2 . 2, 6 / 3, 1 . 3, 3 . 3, 5 . 3, 7 / 4, 4
+    static String[][] magicStar;
+    static int[][] alphabetAxis;    // 알파벳이 들어가는 인덱스: 0, 4 / 1, 1 . 1, 3 . 1, 5 . 1, 7 / 2, 2 . 2, 6 / 3, 1 . 3, 3 . 3, 5 . 3, 7 / 4, 4
+    static boolean[] usedAlphabet;  // 1부터 12까지 쓴 알파벳 체크
 
     public static void main(String[] args) throws IOException {
-        int initCount = init();
-        dfs(initCount, );
+        init();
+//        dfs(initCount, );
     }
 
-    static int init() throws IOException { // . 과 x를 제외한 알파벳들은 우선 숫자로 저장
-        int initCount = 0;  // 최초에 입력돼있는 알파벳 개수를 카운트하는 변수
+    static void init() throws IOException { // . 과 x를 제외한 알파벳들은 우선 숫자로 저장
         magicStar = new String[5][9];
+        alphabetAxis = new int[12][2];
+        usedAlphabet = new boolean[13];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int axisIndex = 0;  // 알파벳 좌표를 입력하기 위한 변수
         for (int i = 0; i < 5; i++) {
             String row = br.readLine();
             for (int j = 0; j < row.length(); j++) {
-                if (!String.valueOf(row.charAt(j)).equals(".") && !String.valueOf(row.charAt(j)).equals("x")) initCount += 1;
+                if (!String.valueOf(row.charAt(j)).equals(".")) {
+                    if (!String.valueOf(row.charAt(j)).equals("x")) usedAlphabet[Integer.parseInt(String.valueOf(row.charAt(j) - 64))] = true;
+                    alphabetAxis[axisIndex] = new int[] {i, j};
+                    axisIndex += 1;
+                }
                 magicStar[i][j] = String.valueOf(row.charAt(j));
             }
         }
-        
-        return initCount;
     }
 
-    static boolean isTwentySix(String[][] drawingStar) {
-        
-        return false;
+    static boolean isTwentySix(String[][] drawingStar) {    // 모든 줄의 합이 26인지 체크
+        if (Integer.parseInt(drawingStar[0][4]) + Integer.parseInt(drawingStar[1][3]) + Integer.parseInt(drawingStar[2][2]) + Integer.parseInt(drawingStar[3][1]) != 26) return false;
+        if (Integer.parseInt(drawingStar[0][4]) + Integer.parseInt(drawingStar[1][5]) + Integer.parseInt(drawingStar[2][6]) + Integer.parseInt(drawingStar[3][7]) != 26) return false;
+        if (Integer.parseInt(drawingStar[3][1]) + Integer.parseInt(drawingStar[3][3]) + Integer.parseInt(drawingStar[3][5]) + Integer.parseInt(drawingStar[3][7]) != 26) return false;
+        if (Integer.parseInt(drawingStar[4][4]) + Integer.parseInt(drawingStar[3][3]) + Integer.parseInt(drawingStar[2][2]) + Integer.parseInt(drawingStar[1][1]) != 26) return false;
+        if (Integer.parseInt(drawingStar[4][4]) + Integer.parseInt(drawingStar[3][5]) + Integer.parseInt(drawingStar[2][6]) + Integer.parseInt(drawingStar[1][1]) != 26) return false;
+
+        return Integer.parseInt(drawingStar[1][1]) + Integer.parseInt(drawingStar[1][3]) + Integer.parseInt(drawingStar[1][5]) + Integer.parseInt(drawingStar[1][7]) == 26;
     }
 
     static void dfs(int count, String[][] drawingStar) {    // count: 몇 개의 알파벳을 넣었는지, drawingStar: 그려나가고 있는 매직스타
@@ -39,8 +50,13 @@ public class Main_골드5_3967_골드스타 {
             if (isTwentySix(drawingStar)) {
                 for (int i = 0; i < 4; i++) System.out.println(Arrays.toString(drawingStar[i]));
             }
+
+            return;
         }
 
+        for (int i = 0; i < usedAlphabet.length; i++) {
+
+        }
         
     }
 
