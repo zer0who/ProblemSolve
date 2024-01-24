4847b3a9b9aaeaa13331ab39f56fa4f8727c2dcb
김영후 <eagle7449@naver.com>
2024-01-24 23:14:54 +0900

    매직스타

--- java/SSAFY/study/algo/week24/Main_골드5_3967_골드스타.java
+++ java/SSAFY/study/algo/week24/Main_골드5_3967_골드스타.java
@@ -3,61 +3,73 @@ package SSAFY.study.algo.week24;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main_골드5_3967_골드스타 {
 
-    static String[][] magicStar;
-    static int[][] alphabetAxis;    // 알파벳이 들어가는 인덱스: 0, 4 / 1, 1 . 1, 3 . 1, 5 . 1, 7 / 2, 2 . 2, 6 / 3, 1 . 3, 3 . 3, 5 . 3, 7 / 4, 4
+    static char[][] magicStar;
+    static List<int[]> blankAxis;   // 알파벳 채워야하는 곳
     static boolean[] usedAlphabet;  // 1부터 12까지 쓴 알파벳 체크
+    static boolean answerFinded;    // 가장 빨리 찾아낸 게 답이므로 true가 되면 dfs 안들어감
 
     public static void main(String[] args) throws IOException {
         init();
-//        dfs(initCount, );
+        dfs(0);
     }
 
     static void init() throws IOException { // . 과 x를 제외한 알파벳들은 우선 숫자로 저장
-        magicStar = new String[5][9];
-        alphabetAxis = new int[12][2];
-        usedAlphabet = new boolean[13];
+        magicStar = new char[5][9];
+        blankAxis = new ArrayList<>();
+        usedAlphabet = new boolean[12];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int axisIndex = 0;  // 알파벳 좌표를 입력하기 위한 변수
         for (int i = 0; i < 5; i++) {
             String row = br.readLine();
             for (int j = 0; j < row.length(); j++) {
-                if (!String.valueOf(row.charAt(j)).equals(".")) {
-                    if (!String.valueOf(row.charAt(j)).equals("x")) usedAlphabet[Integer.parseInt(String.valueOf(row.charAt(j) - 64))] = true;
-                    alphabetAxis[axisIndex] = new int[] {i, j};
-                    axisIndex += 1;
-                }
-                magicStar[i][j] = String.valueOf(row.charAt(j));
+                if (String.valueOf(row.charAt(j)).equals("x")) blankAxis.add(new int[] {i, j});
+                else if (!String.valueOf(row.charAt(j)).equals(".")) usedAlphabet[row.charAt(j) - 65] = true;
+                magicStar[i][j] = row.charAt(j);
             }
         }
+        answerFinded = false;
     }
 
-    static boolean isTwentySix(String[][] drawingStar) {    // 모든 줄의 합이 26인지 체크
-        if (Integer.parseInt(drawingStar[0][4]) + Integer.parseInt(drawingStar[1][3]) + Integer.parseInt(drawingStar[2][2]) + Integer.parseInt(drawingStar[3][1]) != 26) return false;
-        if (Integer.parseInt(drawingStar[0][4]) + Integer.parseInt(drawingStar[1][5]) + Integer.parseInt(drawingStar[2][6]) + Integer.parseInt(drawingStar[3][7]) != 26) return false;
-        if (Integer.parseInt(drawingStar[3][1]) + Integer.parseInt(drawingStar[3][3]) + Integer.parseInt(drawingStar[3][5]) + Integer.parseInt(drawingStar[3][7]) != 26) return false;
-        if (Integer.parseInt(drawingStar[4][4]) + Integer.parseInt(drawingStar[3][3]) + Integer.parseInt(drawingStar[2][2]) + Integer.parseInt(drawingStar[1][1]) != 26) return false;
-        if (Integer.parseInt(drawingStar[4][4]) + Integer.parseInt(drawingStar[3][5]) + Integer.parseInt(drawingStar[2][6]) + Integer.parseInt(drawingStar[1][1]) != 26) return false;
+    static boolean isTwentySix() {    // 모든 줄의 합이 26인지 체크
+        if ((magicStar[0][4]-64) + (magicStar[1][3]-64) + (magicStar[2][2]-64) + (magicStar[3][1]-64) != 26) return false;
+        if ((magicStar[0][4]-64) + (magicStar[1][5]-64) + (magicStar[2][6]-64) + (magicStar[3][7]-64) != 26) return false;
+        if ((magicStar[3][1]-64) + (magicStar[3][3]-64) + (magicStar[3][5]-64) + (magicStar[3][7]-64) != 26) return false;
+        if ((magicStar[4][4]-64) + (magicStar[3][3]-64) + (magicStar[2][2]-64) + (magicStar[1][1]-64) != 26) return false;
+        if ((magicStar[4][4]-64) + (magicStar[3][5]-64) + (magicStar[2][6]-64) + (magicStar[1][7]-64) != 26) return false;
 
-        return Integer.parseInt(drawingStar[1][1]) + Integer.parseInt(drawingStar[1][3]) + Integer.parseInt(drawingStar[1][5]) + Integer.parseInt(drawingStar[1][7]) == 26;
+        return (magicStar[1][1]-64) + (magicStar[1][3]-64) + (magicStar[1][5]-64) + (magicStar[1][7]-64) == 26;
     }
 
-    static void dfs(int count, String[][] drawingStar) {    // count: 몇 개의 알파벳을 넣었는지, drawingStar: 그려나가고 있는 매직스타
-        if (count == 12) {
-            if (isTwentySix(drawingStar)) {
-                for (int i = 0; i < 4; i++) System.out.println(Arrays.toString(drawingStar[i]));
+    static void dfs(int count) {    // count: 몇 개의 알파벳을 넣었는지, drawingStar: 그려나가고 있는 매직스타
+        if (answerFinded) return;
+        if (count == blankAxis.size()) {
+            if (isTwentySix()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 5; i++) {
+                    for (int j = 0; j < 9; j++) sb.append(magicStar[i][j]).append("");
+                    sb.append("\n");
+                }
+                System.out.println(sb);
+                answerFinded = true;
             }
 
             return;
         }
 
         for (int i = 0; i < usedAlphabet.length; i++) {
-
+            if (usedAlphabet[i]) continue;  // 사용한 알파벳이면 패스
+            int[] blank = blankAxis.get(count);
+            magicStar[blank[0]][blank[1]] = (char) (i+65);
+            usedAlphabet[i] = true;
+            dfs(count + 1);
+            magicStar[blank[0]][blank[1]] = '.';
+            usedAlphabet[i] = false;
         }
-        
     }
 
 }
