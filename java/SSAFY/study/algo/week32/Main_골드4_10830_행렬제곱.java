fd28e33bdd2ba4e71ffe80178da85565232e4cab
김영후 <eagle7449@naver.com>
2024-03-24 23:06:23 +0900

    행렬 제곱

--- java/SSAFY/study/algo/week32/Main_골드4_10830_행렬제곱.java
+++ java/SSAFY/study/algo/week32/Main_골드4_10830_행렬제곱.java
@@ -9,18 +9,28 @@ import java.util.StringTokenizer;
 public class Main_골드4_10830_행렬제곱 {
 
     static int N;
-    static int B;
+    static long B;
     static int[][] originMatrix;
+    static final int MOD = 1_000;
 
     public static void main(String[] args) throws IOException {
         init();
+        StringBuilder sb = new StringBuilder();
+        int[][] answer = doPow(originMatrix, B);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(answer[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 
     static void init () throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
+        B = Long.parseLong(st.nextToken());
         originMatrix = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -30,10 +40,28 @@ public class Main_골드4_10830_행렬제곱 {
         }
     }
 
-    static int[] pow(int[] matrix, int powerNumber) {
+    static int[][] multiplyMatrix(int[][] inputMatrix1, int[][] inputMatrix2) { // 행렬 곱 -> aij = bik * bkj
+        int[][] retMatrix = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    retMatrix[i][j] += inputMatrix1[i][k] * inputMatrix2[k][j];
+                    retMatrix[i][j] %= MOD;
+                }
+            }
+        }
 
-        return new int[2];
+        return retMatrix;
     }
 
+    static int[][] doPow(int[][] matrix, long powerNumber) {
+        if (powerNumber == 1) return matrix;
+
+        int[][] temp = doPow(matrix, powerNumber/2);
+
+        temp = multiplyMatrix(temp, temp);
+        if (powerNumber % 2 == 0) return temp;
+        else return multiplyMatrix(temp, matrix);
+    }
 
 }
