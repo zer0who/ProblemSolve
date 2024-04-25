4a1d71dee42576da5ecd4ea9f9531d603d0f974d
김영후 <eagle7449@naver.com>
2024-04-25 20:19:52 +0900

    막대기

---
+++ java/SSAFY/study/algo/Main_실버5_1094_막대기.java
@@ -0,0 +1,28 @@
+package SSAFY.study.algo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_실버5_1094_막대기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int stick = 64;
+        int count = 0;
+
+        while (X > 0) {
+            if (stick > X) {
+                stick /= 2;
+
+            } else {
+                X -= stick;
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
