782e65f32563c5e1e9fe59b906476db535525479
김영후 <eagle7449@naver.com>
2024-06-05 18:17:59 +0900

    가르침 temp

--- java/SSAFY/study/algo/week38/Main_골드4_1062_가르침.java
+++ java/SSAFY/study/algo/week38/Main_골드4_1062_가르침.java
@@ -14,6 +14,7 @@ public class Main_골드4_1062_가르침 {
 
     public static void main(String[] args) throws IOException {
         init();
+        System.out.println(calcWords());
     }
 
     static void init() throws IOException {
@@ -23,6 +24,13 @@ public class Main_골드4_1062_가르침 {
         K = Integer.parseInt(st.nextToken());
         wordList = new ArrayList<>();
         for (int i = 0; i < N; i++) wordList.add(br.readLine());
+    }
+
+    static int calcWords() {
+        if (K < 5) return 0;    // anta + tica 에서 애초에 5개의 글자를 필요로 함
+        else if (K == 26) return N;   // 모든 글자를 가르칠 수 있으면 모든 단어를 읽을 수 있음
+
+        return 0;
     }
 
 }
