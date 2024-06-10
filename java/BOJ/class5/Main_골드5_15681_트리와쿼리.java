05d1163ca8c3aaf7ee0f0f204abf8dfc7be9ebb3
김영후 <eagle7449@naver.com>
2024-06-10 16:38:17 +0900

    트리와 쿼리 Edge 객체 제거

--- java/BOJ/class5/Main_골드5_15681_트리와쿼리.java
+++ java/BOJ/class5/Main_골드5_15681_트리와쿼리.java
@@ -10,29 +10,20 @@ import java.util.StringTokenizer;
 
 public class Main_골드5_15681_트리와쿼리 {
 
-    static class Edge {
-        int from;
-        int to;
-
-        public Edge(int from, int to) {
-            this.from = from;
-            this.to = to;
-        }
-    }
-
     static int N, R, Q;
-    static List<List<Edge>> edgeList;
+    static List<List<Integer>> edgeList;
     static int[] queries;
     static int[] nodeCounts;
     static boolean[] isChecked;
-
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         init();
         countNodes(R);
         for (int i = 0; i < Q; i++) {
-            System.out.println(nodeCounts[queries[i]]);
+            sb.append(nodeCounts[queries[i]]).append('\n');
         }
+        System.out.print(sb);
     }
 
     static void init() throws IOException {
@@ -42,30 +33,28 @@ public class Main_골드5_15681_트리와쿼리 {
         R = Integer.parseInt(st.nextToken());
         Q = Integer.parseInt(st.nextToken());
         edgeList = new ArrayList<>();
-        for (int i = 0; i < N+1; i++) edgeList.add(new ArrayList<>());
-        for (int i = 0; i < N-1; i++) {
+        for (int i = 0; i < N + 1; i++) edgeList.add(new ArrayList<>());
+        for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            edgeList.get(from).add(new Edge(from, to)); // 무향 트리이므로 양쪽 모두에 넣어줌
-            edgeList.get(to).add(new Edge(to, from));
+            edgeList.get(from).add(to);
+            edgeList.get(to).add(from);
         }
         queries = new int[Q];
         for (int i = 0; i < Q; i++) queries[i] = Integer.parseInt(br.readLine());
-        nodeCounts = new int[N+1];
-        isChecked = new boolean[N+1];
+        nodeCounts = new int[N + 1];
+        isChecked = new boolean[N + 1];
     }
 
-    static void countNodes(int nodeNumber) {  // 각 서브트리에 속한 노드의 개수를 카운트하기 위한 함수, 루트부터 시작하여 재귀적으로 동작하게.
+    static void countNodes(int nodeNumber) {
         nodeCounts[nodeNumber] = 1; // 자신도 트리의 정점 중 하나이므로 1부터 시작
         isChecked[nodeNumber] = true;
-        List<Edge> nowChilds = edgeList.get(nodeNumber);
-        for (int i = 0; i < nowChilds.size(); i++) {
-            Edge nowChildEdge = nowChilds.get(i);
-            int child = nowChildEdge.to;
-            if (isChecked[child]) continue;
-            countNodes(child);
-            nodeCounts[nodeNumber] += nodeCounts[child];
+        for (int child : edgeList.get(nodeNumber)) {
+            if (!isChecked[child]) {
+                countNodes(child);
+                nodeCounts[nodeNumber] += nodeCounts[child];
+            }
         }
     }
 
