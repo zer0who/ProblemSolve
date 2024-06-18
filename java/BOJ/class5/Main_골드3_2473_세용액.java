9a527b42ca05f0d1b26677ee0d3d9a3eafafe096
김영후 <eagle7449@naver.com>
2024-06-18 18:08:09 +0900

    세 용액

---
+++ java/BOJ/class5/Main_골드3_2473_세용액.java
@@ -0,0 +1,61 @@
+package BOJ.class5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_골드3_2473_세용액 {
+
+    static int N;
+    static int[] liquids;
+    static long minDiff;
+    static int[] minDiffNumbers;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        Arrays.sort(minDiffNumbers);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) sb.append(minDiffNumbers[i]).append(" ");
+        System.out.println(sb);
+    }
+
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        liquids = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) liquids[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(liquids);
+        minDiff = Long.MAX_VALUE;
+        minDiffNumbers = new int[3];
+    }
+
+    static void solve() {
+        for (int i = 0; i < N; i++) {
+            int baseNumber = liquids[i];
+            int left = 0;
+            int right = N-1;
+            int leftNumber, rightNumber;
+            while (true) {
+                if (left == i) left += 1;
+                else if (right == i) right -= 1;
+                if (left >= right) break;
+                leftNumber = liquids[left];
+                rightNumber = liquids[right];
+                long tempSum = (long) baseNumber + (long) leftNumber + (long) rightNumber;
+                if (Math.abs(tempSum) < minDiff) {
+                    minDiff = Math.abs(tempSum);
+                    minDiffNumbers = new int[] {baseNumber, leftNumber, rightNumber};
+                    if (minDiff == 0) return;   // 합이 0이면 그냥 리턴
+                }
+                if (tempSum < 0) left += 1;
+                else if (tempSum > 0) right -= 1;
+            }
+
+        }
+    }
+
+}
