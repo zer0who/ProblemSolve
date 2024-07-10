6e5e7da13dbfea0b0f3e1202d7e00a0c0264b3f5
김영후 <eagle7449@naver.com>
2024-07-10 17:49:58 +0900

    플래그 대사 개빼낌

--- java/SSAFY/study/algo/week9/Main_골드2_27450_플래그대사그만좀말해요.java
+++ java/SSAFY/study/algo/week9/Main_골드2_27450_플래그대사그만좀말해요.java
@@ -1,73 +1,61 @@
 package SSAFY.study.algo.week9;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_골드2_27450_플래그대사그만좀말해요 {
 
-    private static long totalYellCount;	// 소리친 횟수
-    private static int hanbyulIdx;	// 한별이 위치
+    static int N, K;
+    static int[] initStrength;  // 처음 주어지는 강함 수치
+    static int[] goalStrength;  // 목표 강함 수치
 
-    private static int N;
-    private static int K;
-    private static long[] enemies;	// 적들의 강함
-    private static long[] enemiesGoal;	// 적들의 목표 강함
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         init();
-        doFlagScript();
-        System.out.println(totalYellCount);
+        calcShout();
     }
 
-    private static void init() throws Exception {
-        totalYellCount = 0;
-        hanbyulIdx = 0;
+    static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        enemies = new long[N];
-        enemiesGoal = new long[N];
+        initStrength = new int[N];
+        goalStrength = new int[N];
         st = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            enemies[i] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            enemiesGoal[i] = Integer.parseInt(st.nextToken());
+            initStrength[i] = Integer.parseInt(st.nextToken());
+            goalStrength[i] = Integer.parseInt(st2.nextToken());
         }
     }
 
-    private static void doFlagScript() {
-        while (hanbyulIdx < N) {	// 끝 적한테 갈 때까지
-            long enemy = enemies[hanbyulIdx];	// 현재 한별이가 위치한 곳의 적
-            long enemyGoal = enemiesGoal[hanbyulIdx];	// 그 적의 목표 세기
-            if (enemy >= enemyGoal) {	// 그 위치의 적이 목표 세기를 달성했다면 다음 적으로
-                hanbyulIdx += 1;
-                continue;
+    static void calcShout() {
+        long answer = 0;
+
+        long[] shout = new long[N]; // 인덱스 별 부하들을 강화시키기 위해 필요한 외침의 횟수 저장
+        long diff, neededCount, inc, cnt = 0, inh = 0;   // 현재 위치의 부하를 강화시키기 위해 필요한 숫자, 그 숫자만큼 강화하기 위한 외침의 횟수
+        for (int i = 0; i < N; i++) {
+            if (i > 0) inh -= cnt;  // 두 번째 부하부터는 한 칸 움직일 때마다 이전의 외침 횟수는 강화에 영향을 주지 않음
+            if (i >= K) {
+                cnt -= shout[i-K];  // K칸 이전의 외침은 현재 부하에게 영향을 끼치지 않음
+                inh += K * shout[i-K];
             }
-            yell();
+
+            inc = cnt * K + inh;    // 이전까지 외침으로 인해 증가된 강함
+            diff = (goalStrength[i] - initStrength[i]) - inc;
+            if (diff > 0) { // 강화시키기 위해 외침이 필요한 경우
+                neededCount = (diff % K == 0) ? diff/K : diff/K + 1;
+
+                answer += neededCount;
+                shout[i] += neededCount;   // 외침 필요한 개수만큼 더해 줌
+                cnt += neededCount;
+            }
         }
-    }
 
-    private static void yell() {	// 소리 지르기
-        int yellIdx = hanbyulIdx;	// 목소리의 위치
-        int yellStrong = K;	// 처음 목소리의 세기
-
-        long haveToYellCount = enemiesGoal[hanbyulIdx] - enemies[hanbyulIdx];
-        if (haveToYellCount <= 0) return;   // 해당 적이 이미 목표 이상의 세기를 가진 경우 함수 종료
-        long yellCount = haveToYellCount / K;	// 소리치는 첫 위치에서 소리쳐야하는 횟수
-        if (yellCount == 0) yellCount = 1;	// 목표까지 남은 강함이 한별이 목소리보다 작을 수도 있음 그럴 때는 1로
-
-        while (yellStrong > 0) {	// 목소리 0되기 전까지
-            if (yellIdx >= N) break;
-            enemies[yellIdx] += yellStrong * yellCount;
-
-            yellIdx += 1;
-            yellStrong -= 1;
-        }
-        totalYellCount += yellCount;
+        System.out.println(answer);
     }
 
 }
