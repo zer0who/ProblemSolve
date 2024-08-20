62d23f5608afbb3aef2730f4c9933d083b37e5c1
김영후 <eagle7449@naver.com>
2024-08-20 15:21:11 +0900

    주석 추가

--- java/SSAFY/study/algo/week49/Main_6198_옥상정원꾸미기.java
+++ java/SSAFY/study/algo/week49/Main_6198_옥상정원꾸미기.java
@@ -25,15 +25,15 @@ public class Main_6198_옥상정원꾸미기 {
     }
 
     static void countCanSee() {
-        long answer = 0;
+        long answer = 0;    // 접근 방식: "현재 빌딩을 볼 수 있는" 빌딩을 카운트 하기
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty()) {
-                if (heights[i] < stack.peek()) break;
-                stack.pop();
+        for (int i = 0; i < N; i++) {   // 모든 건물에 대해 수행
+            while (!stack.isEmpty()) {  // 스택이 비거나
+                if (heights[i] < stack.peek()) break;   // 지금 빌딩보다 높은 빌딩이 나올때까지
+                stack.pop();    // 스택에 있는 빌딩들 모두 제거
             }
-            answer += stack.size();
-            stack.push(heights[i]);
+            answer += stack.size(); // 그 후 스택에 남아있는 빌딩이 현재 빌딩을 볼 수 있는 빌딩들임
+            stack.push(heights[i]); // 그 후 해당 건물 스택에 넣어줌
         }
 
         System.out.println(answer);
