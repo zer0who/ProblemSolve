988c7912e1292067ce548bfe4180371dfc4d9324
김영후 <eagle7449@naver.com>
2024-09-14 15:11:38 +0900

    별 가두기 초기화

---
+++ java/SSAFY/study/algo/week53/Main_30679_별가두기.java
@@ -0,0 +1,33 @@
+package SSAFY.study.algo.week53;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_30679_별가두기 {
+
+    static int N, M;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        for (int i = 0; i < N; i++) System.out.println(Arrays.toString(map[i]));
+    }
+
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+}
