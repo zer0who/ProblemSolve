47e2ac14eb7bb690b92e8154363c2ad963211de2
김영후 <eagle7449@naver.com>
2024-09-17 15:04:37 +0900

    수고르기

---
+++ java/SSAFY/study/algo/week53/Main_18430_무기공학.java
@@ -0,0 +1,9 @@
+package SSAFY.study.algo.week53;
+
+public class Main_18430_무기공학 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ java/SSAFY/study/algo/week53/Main_2230_수고르기.java
@@ -0,0 +1,46 @@
+package SSAFY.study.algo.week53;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_2230_수고르기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(A);
+        findMinTwo(M, A);
+    }
+
+    static void findMinTwo(int M, int[] A) {
+        int minDiff = Integer.MAX_VALUE;
+        int left = 0;
+        int right = 0;  // 같은 수를 골랐을 수도 있다고 했으므로 0부터 시작
+
+        int leftNumber, rightNumber;
+        while (left < A.length) {
+            leftNumber = A[left];
+            rightNumber = A[right];
+
+            if (rightNumber - leftNumber >= M) {
+                minDiff = Math.min(minDiff, rightNumber-leftNumber);
+                left++;
+            } else {
+                if (right < A.length - 1) right++;
+                else left++;
+            }
+        }
+
+        System.out.println(minDiff);
+    }
+
+}
