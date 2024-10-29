1897f04311553909c18eb5381a94981bfa38b8d4
김영후 <eagle7449@naver.com>
2024-10-29 23:48:38 +0900

    진우의 민트초코우유

--- java/SSAFY/study/algo/week50s/week59/Main_20208_진우의민트초코우유.java
+++ java/SSAFY/study/algo/week50s/week59/Main_20208_진우의민트초코우유.java
@@ -13,20 +13,26 @@ public class Main_20208_진우의민트초코우유 {
         int row;
         int col;
         int hp;
+        int milkCount;
 
-        public Jinu(int row, int col, int hp) {
+        public Jinu(int row, int col, int hp, int milkCount) {
             this.row = row;
             this.col = col;
             this.hp = hp;
+            this.milkCount = milkCount;
         }
     }
 
     static int N, M, H;
     static int[] house; // 집 좌표
     static List<int[]> milks;   // 초코 우유 좌표가 들어 있는 리스트
+    static int maxDrunkMilk;
 
     public static void main(String[] args) throws IOException {
         init();
+        boolean[][] isDrunken = new boolean[N][N];
+        doEatMilk(new Jinu(house[0], house[1], M, 0), isDrunken);
+        System.out.println(maxDrunkMilk);
     }
 
     static void init() throws IOException {
@@ -45,6 +51,25 @@ public class Main_20208_진우의민트초코우유 {
                 else if (info == 1) house = new int[] {i, j};
             }
         }
+        maxDrunkMilk = 0;
+    }
+
+    static void doEatMilk(Jinu now, boolean[][] isDrunken) {
+        if ((now.row == house[0] && now.col == house[1]) && isDrunken[house[0]][house[1]]) {    // 기저, 위치가 집이고 우유를 하나라도 마시려고 시도한 후에 집에 온 것인 경우
+            if (now.hp < 0) return; // 근데 체력이 0 미만이면 돌아올 수 없는 경우니까 그냥 함수 종료
+            maxDrunkMilk = Math.max(maxDrunkMilk, now.milkCount);
+            return;
+        }
+        int dist;
+        for (int[] milk : milks) {    // 모든 우유들에 대해서
+            dist = Math.abs(now.row - milk[0]) + Math.abs(now.col - milk[1]);
+            if (dist > now.hp || isDrunken[milk[0]][milk[1]]) continue;    // 마실 수 없는 우유이거나 이미 마신 우유라면 건너 뜀
+            isDrunken[milk[0]][milk[1]] = true;
+            doEatMilk(new Jinu(milk[0], milk[1], now.hp-dist+H, now.milkCount+1), isDrunken);
+            isDrunken[milk[0]][milk[1]] = false;
+        }
+        isDrunken[house[0]][house[1]] = true;
+        doEatMilk(new Jinu( house[0], house[1], now.hp - (Math.abs(now.row - house[0]) + Math.abs(now.col - house[1])), now.milkCount ), isDrunken);
     }
 
 }
