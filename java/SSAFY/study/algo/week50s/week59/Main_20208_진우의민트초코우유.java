45e10d26193d5e637c67955d5a8b38af21119206
김영후 <eagle7449@naver.com>
2024-10-29 23:57:36 +0900

    시간 개선

--- java/SSAFY/study/algo/week50s/week59/Main_20208_진우의민트초코우유.java
+++ java/SSAFY/study/algo/week50s/week59/Main_20208_진우의민트초코우유.java
@@ -55,21 +55,29 @@ public class Main_20208_진우의민트초코우유 {
     }
 
     static void doEatMilk(Jinu now, boolean[][] isDrunken) {
-        if ((now.row == house[0] && now.col == house[1]) && isDrunken[house[0]][house[1]]) {    // 기저, 위치가 집이고 우유를 하나라도 마시려고 시도한 후에 집에 온 것인 경우
-            if (now.hp < 0) return; // 근데 체력이 0 미만이면 돌아올 수 없는 경우니까 그냥 함수 종료
-            maxDrunkMilk = Math.max(maxDrunkMilk, now.milkCount);
-            return;
+//        if ((now.row == house[0] && now.col == house[1]) && isDrunken[house[0]][house[1]]) {    // 기저, 위치가 집이고 우유를 하나라도 마시려고 시도한 후에 집에 온 것인 경우
+//            if (now.hp < 0) return; // 근데 체력이 0 미만이면 돌아올 수 없는 경우니까 그냥 함수 종료
+//            maxDrunkMilk = Math.max(maxDrunkMilk, now.milkCount);
+//            return;
+//        }
+        if (now.milkCount > 0) {    // 우유를 하나라도 마신 경우 집에 되돌려 보내봄
+            if (calcDist(new int[] {now.row, now.col}, house) <= now.hp) maxDrunkMilk = Math.max(maxDrunkMilk, now.milkCount);  // 집에 되돌아갈 수 있다면 우유 개수 갱신
+            if (now.milkCount == milks.size()) return;  // 모든 우유를 다 마신 경우 함수 종료
         }
         int dist;
         for (int[] milk : milks) {    // 모든 우유들에 대해서
-            dist = Math.abs(now.row - milk[0]) + Math.abs(now.col - milk[1]);
+            dist = calcDist(new int[] {now.row, now.col}, milk);
             if (dist > now.hp || isDrunken[milk[0]][milk[1]]) continue;    // 마실 수 없는 우유이거나 이미 마신 우유라면 건너 뜀
             isDrunken[milk[0]][milk[1]] = true;
             doEatMilk(new Jinu(milk[0], milk[1], now.hp-dist+H, now.milkCount+1), isDrunken);
             isDrunken[milk[0]][milk[1]] = false;
         }
-        isDrunken[house[0]][house[1]] = true;
-        doEatMilk(new Jinu( house[0], house[1], now.hp - (Math.abs(now.row - house[0]) + Math.abs(now.col - house[1])), now.milkCount ), isDrunken);
+//        isDrunken[house[0]][house[1]] = true;   // 다시 집으로 되돌려 보내기
+//        doEatMilk(new Jinu( house[0], house[1], now.hp - (calcDist(new int[] {now.row, now.col}, house)), now.milkCount ), isDrunken);
+    }
+
+    static int calcDist(int[] p1, int[] p2) {   // 두 좌표의 맨해튼 거리 계산 함수
+        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
     }
 
 }
