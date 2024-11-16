3dc67a2f94ce86f332a67012cc0c81eb3f6fd7b3
김영후 <eagle7449@naver.com>
2024-11-16 15:56:18 +0900

    동전 문제 임시

---
+++ java/SSAFY/study/algo/week60s/week62/Main_1398_동전문제.java
@@ -0,0 +1,24 @@
+package SSAFY.study.algo.week60s.week62;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main_1398_동전문제 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            long price = Long.parseLong(br.readLine());    // 초콜릿의 가격
+
+        }
+        System.out.println(sb);
+    }
+
+}
